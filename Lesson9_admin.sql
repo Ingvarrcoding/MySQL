a55f71702e0504750742f2c515476fc1c5500e0f
Igor Maslovskiy <47256042+Ingvarrcoding@users.noreply.github.com>
2021-05-22 00:02:46 +0300

    Lesson 9

---
+++ Lesson9_admin.sql
@@ -0,0 +1,20 @@
+-- Создайте двух пользователей которые имеют доступ к базе данных shop. Первому пользователю shop_read должны быть доступны только запросы на чтение данных, второму пользователю shop — любые операции в пределах базы данных shop.
+
+USE shop;
+
+DROP USER IF EXISTS 'shop_reader'@'localhost';
+CREATE USER 'shop_reader'@'localhost' IDENTIFIED WITH sha256_password BY '123';
+GRANT SELECT ON shop_online.* TO 'shop_reader'@'localhost';
+
+INSERT INTO catalogs(name)
+VALUES('New catalog');
+SELECT * FROM catalogs;
+
+DROP USER IF EXISTS 'shop'@'localhost';
+CREATE USER 'shop'@'localhost' IDENTIFIED WITH sha256_password BY '123';
+GRANT ALL ON shop_online.* TO 'shop'@'localhost';
+GRANT GRANT OPTION ON shop_online.* TO 'shop'@'localhost';
+
+INSERT INTO catalogs(name)
+VALUES('New catalog');
+SELECT * FROM catalogs;
---
+++ Lesson9_func.sql
@@ -0,0 +1,43 @@
+-- Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+DROP PROCEDURE IF EXISTS hello;
+delimiter //
+CREATE PROCEDURE hello()
+BEGIN
+	IF(CURTIME() BETWEEN '06:00:00' AND '12:00:00') THEN
+		SELECT 'Доброе утро';
+	ELSEIF(CURTIME() BETWEEN '12:00:00' AND '18:00:00') THEN
+		SELECT 'Добрый день';
+	ELSEIF(CURTIME() BETWEEN '18:00:00' AND '00:00:00') THEN
+		SELECT 'Добрый вечер';
+	ELSE
+		SELECT 'Доброй ночи';
+	END IF;
+END //
+delimiter ;
+
+CALL hello();
+
+-- В таблице products есть два текстовых поля: name с названием товара и description с его описанием. Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.
+
+USE shop;
+
+DROP TRIGGER IF EXISTS nullTrigger;
+delimiter //
+CREATE TRIGGER nullTrigger BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+	IF(ISNULL(NEW.name) AND ISNULL(NEW.description)) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Trigger Warning! NULL in both fields!';
+	END IF;
+END //
+delimiter ;
+
+INSERT INTO products (name, description, price, catalog_id)
+VALUES (NULL, NULL, 5000, 2);
+
+INSERT INTO products (name, description, price, catalog_id)
+VALUES ("AMD Ryzen 5 3600", NULL, 15000, 12);
+
+INSERT INTO products (name, description, price, catalog_id)
+VALUES ("AMD Ryzen 5 3600", "Процессор", 15000, 12);
